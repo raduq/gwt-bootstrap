e62b8e05c48219761df967ef421b5cef69bfe5c5
raduq <raduq@raduq-Dell-System-XPS-L502X>
2012-01-25 21:36:35 -0200

    teste

--- src/main/java/com/geekvigarista/gwt/bootstrap/client/ui/Container.java
+++ src/main/java/com/geekvigarista/gwt/bootstrap/client/ui/Container.java
@@ -4,7 +4,7 @@ import com.geekvigarista.gwt.bootstrap.client.ui.resources.BootstrapCssResources
 
 /**
  * A container component. Used in {@link Topbar}
- * @author Carlos Alexandro Becker
+ * @author Carlos Alexandro Becker 
  * @since 25/01/2012
  */
 public class Container extends ContainerBase {
